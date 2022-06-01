57366cc69bf33618a01460a040d04ceb3678c8aa
David Castañeda <garcia.castaneda@hotmail.com>
2022-06-01 13:23:02 -0500

    Add Eureka Service

--- user-service/src/main/java/com/usuario/service/UserServiceApplication.java
+++ user-service/src/main/java/com/usuario/service/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients   //_Habilita el cliente Feing para consumir otros microservicios (urls)
 @SpringBootApplication
+@EnableEurekaClient  //Activación de este microservicio para que se registre en Eureka
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
