6fa90da7bd27ae11c3b0d4974bee94dc08c2a20b
David Castañeda <garcia.castaneda@hotmail.com>
2022-06-02 11:37:50 -0500

    corrección de yaml v2

--- user-service/src/main/java/com/usuario/service/feingclients/MotoFeingClient.java
+++ user-service/src/main/java/com/usuario/service/feingclients/MotoFeingClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.usuario.service.models.Moto;
 
-@FeignClient(name = "moto-service", url = "http://localhost:8083")
+@FeignClient(name = "moto-service")
 @RequestMapping("/moto")
 public interface MotoFeingClient {
 
